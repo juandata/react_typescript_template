f349f5081618ad05aedf6af4d3bda2f832d52ab7
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2022-06-22 11:00:54 -0500

    storybooktypo fixed on package.json script, removed -s

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,9 @@
-// configures the "preview" iframe that renders the storybook components.
-import { configure } from '@storybook/react';
-import requireContext from 'require-context.macro';
-const req = requireContext('../src', true, /\.stories\.tsx$/);
-function loadStories() {
-  req.keys().forEach(req);
-}
-configure(loadStories, module);
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
